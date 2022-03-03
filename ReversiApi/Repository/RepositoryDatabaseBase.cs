a1a878289dd95f0df53a3fd806f1693b030efbae
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2022-03-03 16:29:30 +0100

    Debug adding entities

--- ReversiApi/Repository/RepositoryDatabaseBase.cs
+++ ReversiApi/Repository/RepositoryDatabaseBase.cs
@@ -17,6 +17,7 @@ public abstract class RepositoryDatabaseBase<T> : IDatabaseRepository<T> where T
     /// <inheritdoc />
     public virtual void Add(T entity)
     {
+        entity.Id = 0;
         this.Context.Add(entity);
         this.Context.SaveChanges();
     }
