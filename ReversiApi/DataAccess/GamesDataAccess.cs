ecdcf5446785b77a8f4939421bfe498f8a24b1f6
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2022-03-04 14:43:09 +0100

    Fix coding standards

--- ReversiApi/DataAccess/GamesDataAccess.cs
+++ ReversiApi/DataAccess/GamesDataAccess.cs
@@ -5,14 +5,13 @@ namespace ReversiApi.DataAccess;
 
 public class GamesDataAccess : DbContext
 {
+    public DbSet<GameEntity> Games { get; set; }
+    public DbSet<PlayerEntity> Players { get; set; }
 
     public GamesDataAccess(DbContextOptions<GamesDataAccess> options) : base(options)
     {
 
     }
-
-    public DbSet<GameEntity> Games { get; set; }
-    public DbSet<PlayerEntity> Players { get; set; }
 
     [ExcludeFromCodeCoverage]
     protected override void OnModelCreating(ModelBuilder modelBuilder)
